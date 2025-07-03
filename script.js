482c9945c4f79b49c27f0da99ce2ce37965cb0f1
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-07-03 23:11:47 +0000

    Aula 06 - Alura

--- script.js
+++ script.js
@@ -12,11 +12,21 @@ function mostraPalavrasChave() {
   campoResultado.textContent = palavrasChave.join(", ");
 }
 
-
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  const frequencias = contaFrequencias(palavras);
 
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
   let frequencias = {};
 
   for (let i of palavras) {
@@ -29,8 +39,5 @@ function processaTexto(texto) {
     }
   }
 
-  console.log(frequencias);
-
-  return palavras;
+  return frequencias;
 }
-
