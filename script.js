d42ea14397dddbb4488147a46861008239879234
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-07-03 14:34:43 +0000

    Aula 02-Alura

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+function mostraPalavrasChave() { 
+  alert("fui clicado!"); 
+} 
