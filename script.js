4b1dd4b216f473671e388cbccde916055434d529
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-07-03 15:20:51 +0000

    Aula 04-alura

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector("#entrada-de-texto").value;
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-    const campoResultado = document.querySelector("#resultado-palavrachave");
+  const campoResultado = document.querySelector("#resultado-palavrachave");
 
-    // Remover espaços extras antes e depois do texto e dividir as palavras
-    const palavras = texto.trim().split(/\s+/);
+  const palavrasChave = processaTexto(texto);
 
-    // Juntar as palavras com vírgula sem espaço
-    campoResultado.textContent = palavras.join(",");
+  campoResultado.textContent = palavrasChave.join(", ");
 }
 
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
 
